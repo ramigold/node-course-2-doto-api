42aa5109a27c45538fec2f063497fd76895eee03
Rami Goldenberg <RGoldenberg@skyreis.loc>
2018-03-12 18:26:22 -0400

    Add POST /todos route and refactore mongoose

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ app.post('/todos', (req, res) =>{
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {app};
 // var newToto = new Todo({
 //   text: 'Cook dinner'
 // });
